8152f0b9cf652344d3e46ffd9448b7bfb1400ebc
Michael Bykovski <mbykovski@seibert-media.net>
2015-03-09 22:12:35 +0100

    Included packages

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='django-fast-sync',
-    version='0.1',
-    packages=[''],
+    version='0.2.1',
+    packages=['django_fast_sync'],
     url='https://github.com/bykof/django-fast-sync',
     license='Apache License',
+    include_package_data=True,
     author='mbykovski',
     author_email='mbykovski@seibert-media.net',
     description='A Django app which helps you to import a lot of data with raw SQL', requires=['django']
